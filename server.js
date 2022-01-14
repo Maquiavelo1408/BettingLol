5c13dd56960cd98b70f25dd4b869843c8705d018
MaquiaveloAlpha <rraygoza1408@gmail.com>
2022-01-14 10:24:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/betting-lol'));
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/BettingLol'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/betting-lol/index.html'));});
+    
+res.sendFile(path.join(__dirname,'/dist/BettingLol/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
